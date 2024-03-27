ad7be986e4f59e4e516ec5d0c326b77b1401e43b
Pavwos <rspavanelli@gmail.com>
2024-03-27 20:28:03 -0300

    teste
    aaa

---
+++ modelo6/my-react-app/src/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Pavwos WebSite</p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ modelo6/my-react-app/src/Header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+
+    return(
+        <header>
+            <h1>Oi gente tudo bem 😘😍</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <hr />
+            </nav>
+        </header>
+    );
+
+}
+
+export default Header
