2f8261a924ccf2cb54d381efac7b341fbdf0c5cf
Pavwos <rspavanelli@gmail.com>
2024-04-02 23:49:10 -0300

    vai

--- modelo6/my-react-app/src/App.jsx
+++ modelo6/my-react-app/src/App.jsx
@@ -1,14 +1,10 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
+import List from './List.jsx'
 
 function App() {
-
   return(
     <>
-      <Header/>
-      <Footer/>
+      <List/>
     </>
-    
   );
 }
 
